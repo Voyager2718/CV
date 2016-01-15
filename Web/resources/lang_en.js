b4d58adac99769567df7482370a9076f08ad49ed
Voyager2718 <voyager2718@gmail.com>
2016-01-16 00:54:04 +0100

    Web CV version 0.5

--- Web/resources/lang_en.js
+++ Web/resources/lang_en.js
@@ -1,11 +1,15 @@
 ﻿var lang =
 {
-    "MyCV": "My CV",
+    "WebCV": "Web CV",
     "LastName": "Last name",
     "FirstName": "First name",
     "Age": "Age",
     "Language": "English",
     "PersonalInfo": "Personal Information",
+    "EducationInfo": "Education Information",
+    "ProfessionalExp": "Professional Experience",
+    "Abilities": "Abilities",
+    "Interests": "Interests",
     "Email": "☏: zhipeng.yang.it@hotmail.com",
     "Clipboard": "Copy to clip board"
 }
